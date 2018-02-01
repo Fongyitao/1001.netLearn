db1e590ea6748109c62b529a81e793a48138e0db
fengyitao <2059634680@qq.com>
2018-02-01 12:58:46 +0800

    1

---
+++ 1001.netLearn/001.进程/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _001.进程
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Process.Start("explorer", "www.baidu.com");
+        }
+    }
+}
