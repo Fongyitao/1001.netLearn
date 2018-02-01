fdd5b05a08665acabd212559bdf255c358a29f92
fengyitao <2059634680@qq.com>
2018-02-01 13:45:00 +0800

    1

--- 1001.netLearn/001.进程/Program.cs
+++ 1001.netLearn/001.进程/Program.cs
@@ -7,8 +7,21 @@ namespace _001.进程
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Process.Start("explorer", "www.baidu.com");
+            //Process.Start("explorer", "https://www.baidu.com");
+            //Process.Start("notepad","test.txt");//有这个文件就打开，没有就创建
+
+            //Process[] pros = Process.GetProcesses();
+            //foreach (var item in pros)
+            //{
+            //    Console.WriteLine(item.ProcessName);
+            //}
+
+            Process pro = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo("test.txt");
+            pro.StartInfo = psi;
+            pro.Start();
+            
+            Console.ReadKey();
         }
     }
 }
---
+++ 1001.netLearn/002_打开文件练习/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace _002_打开文件练习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入要打开文件的路径：");
+            string filePath = Console.ReadLine();
+            Console.WriteLine("请输入要打开的文件名");
+            string fileName = Console.ReadLine();
+
+            BaseFile bf = GetFile(filePath, fileName);
+            if (bf!=null)
+            {
+                bf.OpenFile();
+            }
+
+        }
+        static BaseFile GetFile(string filePath,string fileName)
+        {
+            BaseFile bf = null;
+            string strExtension = Path.GetExtension(fileName);
+            switch (strExtension)
+            {
+                case ".txt":
+                    bf = new TxtFile(filePath, fileName);
+                    break;
+                default:
+                    break;
+            }
+            return bf;
+        }
+    }
+
+    class BaseFile
+    {
+        private string _filePath;
+
+        public string FilePath { get => _filePath; set => _filePath = value; }
+        public string FileName { get; set; }
+
+        public BaseFile(string filePaht,string fileName)
+        {
+            this.FilePath = filePaht;
+            this.FileName = fileName;
+        }
+
+        //设计一个打开文件的函数,//抛异常不知道什么问题
+        public void OpenFile()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo(this.FilePath + "\\" + this.FileName);
+            Process pro = new Process();
+            pro.StartInfo = psi;
+            pro.Start();
+        }
+    }
+    class TxtFile : BaseFile
+    {
+        public TxtFile(string filePaht, string fileName) : base(filePaht, fileName)
+        {
+        }
+    }
+
+
+}
